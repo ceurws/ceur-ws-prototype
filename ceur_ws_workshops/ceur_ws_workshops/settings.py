c438a0d2f6c3802db552c6c92979124be368222e
Filip Muntean <f.muntean@student.vu.nl>
2024-01-17 17:28:34 +0100

    admin panel

--- ceur_ws_workshops/ceur_ws_workshops/settings.py
+++ ceur_ws_workshops/ceur_ws_workshops/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "workshops.apps.WorkshopsConfig", 
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
