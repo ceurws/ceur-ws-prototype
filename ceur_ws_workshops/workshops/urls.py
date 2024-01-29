f437477d3c9dcb6f60e70290dd858df073a4cea6
Filip Muntean <f.muntean@student.vu.nl>
2024-01-29 13:10:34 +0100

    started the basic UI, added fields to create a workshop, paper, author and editor

--- ceur_ws_workshops/workshops/urls.py
+++ ceur_ws_workshops/workshops/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 
 from . import views
+from .views import add_editor, editor_added_success, create_workshop, workshop_created_success, edit_workshop
+
 app_name = "workshops"
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create_workshop/', views.create_workshop, name='create_workshop'),
-    path('submit_paper/', views.submit_paper, name='submit_paper'),
-    # path("", views.IndexView.as_view(), name="index"),
-    # path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    # path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    # path("<int:question_id>/vote/", views.vote, name="vote"),
+    path('add_editor/', add_editor, name='add_editor'),
+    path('editor_added_success/', editor_added_success, name='editor_added_success'),
+    path('create_workshop/', create_workshop, name='create_workshop'),
+    path('workshop_created_success/', workshop_created_success, name='workshop_created_success'),
+    path('add_author/', views.add_author, name='add_author'),
+    path('author_added_success/', views.author_added_success, name='author_added_success'),
+    path ('add_paper/', views.add_paper, name='add_paper'),
+    path ('paper_added_success/', views.paper_added_success, name='paper_added_success'),
+    path('edit_workshop/<int:workshop_id>/', edit_workshop, name='edit_workshop'),
 ]
