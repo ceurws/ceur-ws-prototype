ca71b46f99230df64a85e6d20a3966d9c1a34341
Hein-k <heinkolk@hotmail.com>
2024-02-12 09:29:31 +0100

    implemented new structure discussed during call

--- ceur_ws_workshops/workshops/models.py
+++ ceur_ws_workshops/workshops/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 import uuid
 from django.utils import timezone
-import datetime
 from django.contrib import admin
 
 class Editor(models.Model):
-    volume_editor = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
 class Author(models.Model):
     author_name = models.CharField(max_length=100, null= True, blank=True)
@@ -18,24 +17,17 @@ class Workshop(models.Model):
     license = models.CharField(max_length=50)
     workshop_title = models.CharField(max_length=200)
     location_time = models.CharField(max_length=200)
-    editors = models.ForeignKey(Editor, on_delete=models.CASCADE, null=True, blank=True)
+    editors = models.ManyToManyField(Editor, blank=True)  # Change this line to ManyToManyField
+
     # need to include table of contents 
     # need to include submitted papers 
+    
     submitted_papers = models.ManyToManyField('Paper', related_name='submitted_papers')
     accepted_papers = models.ManyToManyField('Paper', related_name='accepted_papers')
     regular_accepted_papers = models.ManyToManyField('Paper', related_name='regular_accepted_papers')
     short_accepted_papers = models.ManyToManyField('Paper', related_name='short_accepted_papers')
     secret_token = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    # pub_date = models.DateTimeField("date published")
-    # @admin.display(
-    #     boolean=True,
-    #     ordering="pub_date",
-    #     description="Published recently?",
-    # )
-    # def was_published_recently(self):
-    #     now = timezone.now()
-    #     return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    
+
     def __str__(self):
         return self.workshop_title   
 
@@ -44,9 +36,9 @@ class Paper(models.Model):
     paper_title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     pages = models.CharField(max_length=10)
-    # metadata = models.JSONField()  # Assuming metadata is stored as JSON
-    # uploaded_file = models.FileField(upload_to='papers/')
-    # Licensing information fields
+    uploaded_file = models.FileField(upload_to='papers/')  # Add this line for file upload
+    
+    # Add other fields as needed (JSON?)
 
     def __str__(self):
         return self.paper_title
