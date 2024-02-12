bfed775c3b81fedce9a5707bfbd13d7fb80fa4ad
Filip Muntean <f.muntean@student.vu.nl>
2024-02-12 11:10:24 +0100

    now we save the metadata after hitting confirm on the edit page and then land on the page with the 2 urls

--- ceur_ws_workshops/workshops/urls.py
+++ ceur_ws_workshops/workshops/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     path('create_workshop/', create_workshop, name='create_workshop'),
     path('author_upload/<int:workshop_id>/', views.author_upload, name='author_upload'),
     path('edit_workshop/<int:workshop_id>/', edit_workshop, name='edit_workshop'),
+    path('edit_workshop/', edit_workshop, name='edit_workshop'),
     path('create_workshop/<int:workshop_id>/', create_workshop, name='create_workshop'),
     path('workshop_overview/<int:workshop_id>/', workshop_overview, name='workshop_overview'),
     path('author_upload_success/', views.metadata_added_success, name='author_upload_success'),
+    path('workshop_edit_success/<int:workshop_id>/', views.workshop_edit_success, name='workshop_edit_success'),
 ]
