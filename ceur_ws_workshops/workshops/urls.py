05150d3c867748a6d128fdaed535cc025d3415de
Hein-k <heinkolk@hotmail.com>
2024-02-12 09:29:31 +0100

    implemented new structure discussed during call

--- ceur_ws_workshops/workshops/urls.py
+++ ceur_ws_workshops/workshops/urls.py
@@ -1,22 +1,16 @@
 from django.urls import path
 
 from . import views
-from .views import add_editor, editor_added_success, create_workshop, workshop_created_success, edit_workshop, workshop_overview
+from .views import create_workshop, edit_workshop, workshop_overview
 
 app_name = "workshops"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add_editor/', add_editor, name='add_editor'),
-    path('editor_added_success/', editor_added_success, name='editor_added_success'),
     path('create_workshop/', create_workshop, name='create_workshop'),
-    path('workshop_created_success/', workshop_created_success, name='workshop_created_success'),
-    path('add_author/', views.add_author, name='add_author'),
-    path('author_added_success/', views.author_added_success, name='author_added_success'),
-    path ('add_paper/', views.add_paper, name='add_paper'),
-    path ('paper_added_success/', views.paper_added_success, name='paper_added_success'),
+    path('author_upload/<int:workshop_id>/', views.author_upload, name='author_upload'),
     path('edit_workshop/<int:workshop_id>/', edit_workshop, name='edit_workshop'),
     path('create_workshop/<int:workshop_id>/', create_workshop, name='create_workshop'),
-    path('workshop_overview/<int:workshop_id>/', workshop_overview, name='workshop_overview')
-
+    path('workshop_overview/<int:workshop_id>/', workshop_overview, name='workshop_overview'),
+    path('author_upload_success/', views.metadata_added_success, name='author_upload_success'),
 ]
