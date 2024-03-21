25528776f14ffb9573aad5ea640ce7ea65afe76d
filipmuntean <f.muntean@student.vu.nl>
2024-03-21 14:22:16 +0100

    edited admin page to be easier to  read and modify

--- ceur_ws_workshops/workshops/admin.py
+++ ceur_ws_workshops/workshops/admin.py
@@ -1,23 +1,66 @@
 from django.contrib import admin
 from .models import Workshop, Author, Editor, Paper
+from django.forms import TextInput, Textarea, EmailInput, URLInput
+from django.db import models
+from django import forms
+from django.db.models import Q
+
+overrides = {
+    models.CharField: {'widget': TextInput(attrs={'size': '20'})},
+    models.TextField: {'widget': Textarea(attrs={'rows': 4, 'cols': '40'})},
+    models.URLField: {'widget': URLInput(attrs={'size': '50'})},  
+    models.UUIDField: {'widget': TextInput(attrs={'size': '36'})}, 
+    models.EmailField: {'widget': EmailInput(attrs={'size': '30'})},  
+}
 
 class WorkshopAdmin(admin.ModelAdmin):
-    list_display = ('workshop_title', 'publication_year', 'workshop_country')
-    search_fields = ['workshop_title']
+    list_display = [field.name for field in Workshop._meta.fields] + ['get_editors']
+    list_display_links = ('workshop_title',)
+    formfield_overrides = overrides
+    search_fields = [field.name for field in Workshop._meta.fields]
 
+    def get_editors(self, obj):
+        return ", ".join(editor.name for editor in obj.editors.all())
+    get_editors.short_description = 'Editors'
+
+    def get_editor_search_result(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_editor_search_result(request, queryset, search_term)
+        if search_term:
+            editor_query = Q(editors__name__icontains=search_term)
+            queryset |= self.model.objects.filter(editor_query)
+            use_distinct = True  
+        return queryset, use_distinct
+    
 class PaperAdmin(admin.ModelAdmin):
-    list_display = ('paper_title', 'pages')
-    search_fields = ['paper_title', 'author__author']
+    
+    list_display = [field.name for field in Paper._meta.fields] + ['get_authors']
+    list_display_links = ('paper_title',)
+    search_fields = ['paper_title']
+    formfield_overrides = overrides
+
+    def get_authors(self, obj):
+        return ", ".join(author.author_name for author in obj.authors.all())
+    get_authors.short_description = 'Authors'
+
+    def get_author_search_result(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_author_search_result(request, queryset, search_term)
+        if search_term:
+            editor_query = Q(authors__author_name__icontains=search_term)
+            queryset |= self.model.objects.filter(editor_query)
+            use_distinct = True  
+        return queryset, use_distinct
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('author_name', 'id')
+    list_display = [field.name for field in Author._meta.fields]
     search_fields = ['author_name']
+    formfield_overrides = overrides
 
 class EditorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'id')
+    list_display = [field.name for field in Editor._meta.fields]
+    list_display_links = ('name',)
     search_fields = ['name']
-    
-# Register your models here.
+    formfield_overrides = overrides
+
 admin.site.register(Workshop, WorkshopAdmin)
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Editor, EditorAdmin)
--- ceur_ws_workshops/workshops/urls.py
+++ ceur_ws_workshops/workshops/urls.py
@@ -1,29 +1,13 @@
 from django.urls import path
 
 from . import views
-<<<<<<< HEAD
-from .views import create_workshop, workshop_overview, AuthorUpload
-=======
 from .views import CreateWorkshop, AuthorUpload, WorkshopOverview
->>>>>>> 9143bd8a7e798201f437ab50809811c811307797
 
 app_name = "workshops"
 
 urlpatterns = [
     path('', views.index, name='index'),
-<<<<<<< HEAD
-    path('create_workshop/', create_workshop, name='create_workshop'),
-    path('author_upload/<uuid:secret_token>/', AuthorUpload.as_view(), name='author_upload'),
-    path('create_workshop/<int:workshop_id>/', create_workshop, name='create_workshop'),
-    path('workshop_overview/<uuid:secret_token>/', workshop_overview, name='workshop_overview'),
-    path('author_upload_success/<uuid:secret_token>', views.author_upload_check, name='author_upload_success'),
-    # author-edit-form
-    path('workshop_edit_success/<int:workshop_id>/', views.workshop_edit_success, name='workshop_edit_success'),
-    path('author_upload_check/<uuid:secret_token>/', views.author_upload_check, name='author_upload_check'),
-    path('submit_workshop/<uuid:secret_token>/', views.submit_workshop, name='submit_workshop'),
-=======
     path('create_workshop/', CreateWorkshop.as_view(), name='create_workshop'),
     path('author_upload/<uuid:secret_token>/', AuthorUpload.as_view(), name='author_upload'),
     path('workshop_overview/<uuid:secret_token>/', WorkshopOverview.as_view(), name='workshop_overview'),
->>>>>>> 9143bd8a7e798201f437ab50809811c811307797
 ]
