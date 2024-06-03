91c22ebca7bb388c45e66b08f33ff72460a4b90b
Hein-k <heinkolk@hotmail.com>
2024-06-03 11:54:22 +0200

    new commit

---
+++ ceur_ws_workshops/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'ceur_ws_workshops',
+    version = '0.2',
+    description = "Submission process for the CEUR-WS.org workshop proceedings",
+    author = "Filip Muntean, Hein Kolk",
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = [        
+        'asgiref==3.8.1',
+        'crispy-bootstrap4==2024.1',
+        'Django==5.0.6',
+        'django-countries==7.6.1',
+        'django-crispy-forms==2.1',
+        'pip==24.0',
+        'setuptools==70.0.0',
+        'sqlparse==0.5.0',
+        'typing_extensions==4.12.1',
+        'wheel==0.43.0'
+    ],
+    python_requires='>=3.10',
+    entry_points = {
+        'console_scripts': [
+            'ceur_ws_workshops_manage = ceur_ws_workshops.manage:main',
+        ],
+    },
+
+    classifiers = [ 
+        'Framework :: Django',
+        'Framework :: Django :: 3.0', 
+    ],
+
+)
--- ceur_ws_workshops/workshops/views/open_review.py
+++
@@ -1,4 +0,0 @@
-from django.views.generic import View
-
-class OpenReview(View):
-    pass
