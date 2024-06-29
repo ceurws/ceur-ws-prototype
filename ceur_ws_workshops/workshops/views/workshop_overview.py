d4e0400bdecfb922dbc36a20c905063c90b7ecdc
Filip Muntean <f.muntean@student.vu.nl>
2024-06-29 14:58:51 +0200

    editing works on papers, need to fix sorting

--- ceur_ws_workshops/workshops/views/workshop_overview.py
+++ ceur_ws_workshops/workshops/views/workshop_overview.py
@@ -17,7 +17,7 @@ class WorkshopOverview(View):
             'workshop' : workshop,
             'workshop_form': WorkshopForm(instance=workshop),
             # 'paper_forms' : [PaperForm(instance=paper_instance, workshop=workshop) for paper_instance in workshop.accepted_papers.all()],
-            'paper_forms': PaperFormset(queryset = workshop.accepted_papers.all(), prefix = "paper", agreement_not_required=True),
+            'paper_forms': PaperFormset(queryset = workshop.accepted_papers.all(), prefix = "paper",  agreement_not_required = True),
             'paper_forms_no_session' : [paper for paper in workshop.accepted_papers.all() if paper.session == None],
             'session_title_list' : [session_object.session_title for session_object in workshop.sessions.all()],
             'editor_forms': EditorFormSet(queryset=workshop.editors.all(), prefix="editor"),
@@ -53,50 +53,46 @@ class WorkshopOverview(View):
         
         elif request.POST["submit_button"] == "Confirm":
             workshop_form = WorkshopForm(instance=self.get_workshop(), data=request.POST, files = request.FILES)
-            editor_formset = EditorFormSet(request.POST, request.FILES, queryset=workshop.editors.all(), prefix="editor", form_kwargs={'agreement_not_required': True})
-            paper_form = PaperFormset(request.POST, request.FILES, queryset = workshop.accepted_papers.all(), prefix="paper")
-            print(paper_form)
-            print("request.POST", request.POST)
-            print("before if !!!!")
-            if all([workshop_form.is_valid(), editor_formset.is_valid(), paper_form.is_valid()]):
-                print("in if ")
+            editor_formset = EditorFormSet(request.POST, request.FILES, queryset=workshop.editors.all(), prefix="editor")
+            paper_form = PaperFormset(request.POST, request.FILES, queryset = workshop.accepted_papers.all(), prefix="paper", agreement_not_required = True)
+            print('here')
+            if all([workshop_form.is_valid(), editor_formset.is_valid()]):
+                print("then here")
                 workshop_form.save()
                 editor_formset.save()
-                saved_paper_instance = paper_form.save()
-                if request.POST['session'] != '':
-                    saved_session_instance = Session.objects.get(pk=request.POST['session'])
-                    saved_paper_instance.session = saved_session_instance
-                else:
-                    saved_paper_instance.session = None
+            elif paper_form.is_valid():
+                saved_paper_instance = paper_form.save(commit=False)
+                papers_to_delete = request.POST.getlist('papers_to_delete') 
+                for paper_id in papers_to_delete:
+                    Paper.objects.filter(id=paper_id).delete()
 
-                workshop.accepted_papers.add(saved_paper_instance)
+                paper_order = request.POST.getlist('paper_order[]')
+                print(paper_order)
+                saved_paper_instance.order = paper_order
+                saved_paper_instance = paper_form.save()
+
+                # if request.POST['session'] != '': 
+                #     saved_session_instance = Session.objects.get(pk=request.POST['session'])
+                #     saved_paper_instance.session = saved_session_instance
+                # else:
+                #     saved_paper_instance.session = None
             else: 
-                print("paper formset", paper_form.errors)
-                print("NOOOOO")
                 return self.render_workshop(request, edit_mode=True) 
             
             # existing_paper_ids = request.POST.getlist('paper_id')  
-            papers_to_delete = request.POST.getlist('papers_to_delete') 
-            for paper_id in papers_to_delete:
-                Paper.objects.filter(id=paper_id).delete()
+            
 
 
             # for paper_id in existing_paper_ids:
-            #     if paper_id in papers_to_delete:
-            #         continue  
-
-            #     # paper_instance = Paper.objects.filter(id=paper_id, workshop = workshop).first()
-            #     # paper_form = PaperForm(data = request.POST, files = request.FILES, instance=paper_instance, workshop = workshop)
-                
             #     print(paper_form)
                
-            #         order_key = f'order_{paper_id}'
-            #         if order_key in request.POST:
-            #             saved_paper_instance.order = int(request.POST[order_key])
-            #             print(saved_paper_instance.order)
-            #             saved_paper_instance.save()
+                    
             #         print("SUCESSSS")
-            
+            # order_key = f'order_{paper_id}'
+            # if order_key in request.POST:
+            #     saved_paper_instance.order = int(request.POST[order_key])
+            #     print(saved_paper_instance.order)
+            #     saved_paper_instance.save()
 
             return self.render_workshop(request, edit_mode=False)
         elif request.POST["submit_button"] == "Submit Workshop":
